cc5737fe781c74197c9cd60befec1ba1303a5591
Gurleen Singh <learning2989@gmail.com>
2025-11-20 11:17:31 -0500

    updated to hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
 // App entry: mounts React tree with routing support
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename="/portfolio/">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
